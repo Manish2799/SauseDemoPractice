7c527ed302d2d3bdeb8dd181d0a0344ef1b432c9
unknown <Manishgandhi2799@gmail.com>
2025-01-25 19:35:13 +0530

    FrameWork By Manish

--- src/main/java/UtilityLayer/DropdownHandle.java
+++ src/main/java/UtilityLayer/DropdownHandle.java
@@ -1,5 +1,28 @@
 package UtilityLayer;
 
-public class DropdownHandle {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+import BaseLayer.BaseClass;
+
+public class DropdownHandle extends BaseClass{
+
+	public static void HandleDropdown(WebElement wb,String ExpectedValue)
+	{
+		Select sel= new Select(wb);
+		
+		sel.selectByVisibleText(ExpectedValue);
+	}
+	public static void HandleDropdownByValue(WebElement wb,String Value)
+	{
+		Select sel= new Select(wb);
+		
+		sel.selectByValue(Value);
+	}
+	public static void HandleDropdownByIndex(WebElement wb,int indexposition)
+	{
+		Select sel= new Select(wb);
+		
+		sel.selectByIndex(indexposition);
+	}
 }
--- src/main/java/UtilityLayer/WebElementHelper.java
+++ src/main/java/UtilityLayer/WebElementHelper.java
@@ -1,5 +1,21 @@
 package UtilityLayer;
 
-public class WebElementHelper {
+import org.openqa.selenium.WebElement;
 
+import BaseLayer.BaseClass;
+
+public class WebElementHelper extends BaseClass{
+
+	public static void clickOnWebElement(WebElement wb)
+	{
+		wb.click();
+	}
+	
+	public static void sendKeys(WebElement wb,String value)
+	{
+		wb.sendKeys(value);
+	}
+	
+	
+	
 }

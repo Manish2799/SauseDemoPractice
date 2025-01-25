df2fbe63ca71bee9d4b98896da355705d4fb8f12
Aj-journey <abhijeetjadhav.m80@gmail.com>
2025-01-25 20:11:31 +0530

    ProductsPage Functionality By Abhijeet

---
+++ src/main/java/PageLayer/ProductPage.java
@@ -0,0 +1,47 @@
+package PageLayer;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import BaseLayer.BaseClass;
+import UtilityLayer.DropdownHandle;
+import UtilityLayer.Utils;
+import UtilityLayer.WebElementHelper;
+
+public class ProductPage extends BaseClass{
+
+	
+	@FindBy(xpath="//select[@class='product_sort_container']")
+		private WebElement sortProduct;
+	
+	@FindBys(@FindBy(xpath="//button[@class='btn btn_primary btn_small btn_inventory '"))
+	private List<WebElement> productList;
+	
+	@FindBy(id="shopping_cart_container")
+	private WebElement shoppingTrolly;
+	
+	public ProductPage()
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void sortProductByRangeFunctionality(String ProductRange)
+	{
+		DropdownHandle.HandleDropdown(sortProduct, ProductRange);
+	}
+	
+	public void productChoserFunctionality(int number)
+	{
+		Utils.selectProducts(productList, number);
+	}
+	
+	
+	public void shoppingTrollyFunctionality()
+	{
+		WebElementHelper.clickOnWebElement(shoppingTrolly);
+	}
+}
--- src/test/java/StepDefination/LoginStep.java
+++ src/test/java/StepDefination/LoginStep.java
@@ -1,5 +1,32 @@
 package StepDefination;
 
-public class LoginStep {
+import BaseLayer.BaseClass;
+import PageLayer.LoginPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
+public class LoginStep extends BaseClass{
+
+	static LoginPage loginPage;
+
+@Given("user should open browser")
+public void user_should_open_browser() {
+   
+	BaseClass.intialization();
+}
+
+@When("user enter valid Username as {string} and Password as {string}")
+public void user_enter_valid_username_as_and_password_as(String Username, String Password) {
+
+	 loginPage = new LoginPage();
+	
+	loginPage.loginFunctioanlity(Username, Password);
+}
+
+@Then("user click on login button")
+public void user_click_on_login_button() {
+    
+	loginPage.loginButtonFunctionality();
+}
 }
